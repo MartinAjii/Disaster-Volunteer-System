ccfa7ccb4559a4e06bb4620491977dcf681e7391
MartinAjii <martin.aji789@gmail.com>
2026-05-25 20:01:22 +0700

    update login lagi

--- frontend/mobile/mobile_user/lib/core/services/disaster_service.dart
+++ frontend/mobile/mobile_user/lib/core/services/disaster_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../constants/api.dart';
 
 class DisasterService {
@@ -9,16 +10,35 @@ class DisasterService {
 
     try {
 
-      final response = await http.get(
+      final prefs =
+          await SharedPreferences
+              .getInstance();
+
+      final token =
+          prefs.getString("token");
+
+      final response =
+          await http.get(
+
         Uri.parse(
           "${Api.baseUrl}/disasters",
         ),
+
+        headers: {
+
+          "Authorization":
+              "Bearer $token",
+        },
       );
 
-      final data =
-          jsonDecode(response.body);
+      print(response.body);
 
-      if (data["success"] == true) {
+      final data =
+          jsonDecode(
+              response.body);
+
+      if (data["success"] ==
+          true) {
 
         return data["data"];
       }
