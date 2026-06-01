a192c874b57df09a75ff0244dfe07158f5042c3c
MartinAjii <martin.aji789@gmail.com>
2026-06-01 10:15:48 +0700

    fix: config firebase v2

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -3,6 +3,8 @@ const admin = require('firebase-admin');
 const admin = require('firebase-admin');
 const serviceAccount = require('./firebase-service-account.json');
 
+console.log('SERVICE ACCOUNT PROJECT:', serviceAccount.project_id);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
